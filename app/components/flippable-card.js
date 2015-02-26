044a33ccd58f90863426f92ce0fdb32a66d9d34f
Phil Gengler <phil@pgengler.net>
2015-02-26 14:16:01 -0500

    Use 'GFM linebreaks' for rendered markdown

--- app/components/flippable-card.js
+++ app/components/flippable-card.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
 	classNameBindings: [ 'isFlipped:flipped' ],
 	isFlipped: Ember.computed.equal('side', 'back'),
 	side: 'front',
+	markdownOptions: { gfm: true, breaks: true },
 
 	click: function() {
 		var side = this.get('side');
