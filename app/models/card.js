e479f09636c84eb45b185333f297be43acb849ac
Phil Gengler <phil@pgengler.net>
2015-03-11 16:07:06 -0400

    Add card/card set relationship to client side

--- app/models/card.js
+++ app/models/card.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
 	front: DS.attr('string'),
-	back: DS.attr('string')
+	back: DS.attr('string'),
+
+	cardSets: DS.hasMany('cardSet')
 });
