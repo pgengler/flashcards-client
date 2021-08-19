59f0e60cd0a92573550bb488721c4ccecfecf86f
Phil Gengler <phil@pgengler.net>
2021-08-18 22:40:43 -0400

    Implement and use a refreshRoute util with test waiters

--- app/components/top-nav.js
+++ app/components/top-nav.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import refreshRoute from 'flashcards/utils/refresh-route';
 
 export default class TopNav extends Component {
   @service currentCollection;
@@ -22,9 +23,9 @@ export default class TopNav extends Component {
   @action
   randomCard() {
     if (this.router.currentRouteName === 'collection.card.random') {
-      // once RouterService#refresh lands in a release, use that instead
+      // once RouterService#refresh lands in a release, use that instead:
       // this.router.refresh();
-      this.router._router._routerMicrolib.refresh(); // eslint-disable-line ember/no-private-routing-service
+      refreshRoute(this.router);
     } else {
       this.router.transitionTo('collection.card.random', this.collection.slug);
     }
--- app/controllers/collection/card/new.js
+++ app/controllers/collection/card/new.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import refreshRoute from 'flashcards/utils/refresh-route';
 
 export default class CardNewController extends Controller {
   @service router;
@@ -11,10 +12,9 @@ export default class CardNewController extends Controller {
   @action
   onSave(card) {
     if (this.addAnother) {
-      // someday this will be possible:
+      // once RouterService#refresh lands in a release, use that instead:
       // this.router.refresh();
-      // until then, here's a hack:
-      this.router._router._routerMicrolib.refresh(); // eslint-disable-line ember/no-private-routing-service
+      refreshRoute(this.router);
     } else {
       this.router.transitionTo('collection.card.show', card.collection.get('slug'), card.id);
     }
