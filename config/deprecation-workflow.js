2695adcf2d3abd4e2caea885d2cef982718aff82
Phil Gengler <phil@pgengler.net>
2021-12-16 22:38:36 -0500

    WIP to get rid of ember-bootstrap

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,9 +2,9 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: 'silence', matchId: 'ember.built-in-components.import' },
-    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
-    { handler: 'silence', matchId: 'ember-global' },
-    { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'warn', matchId: 'ember.built-in-components.import' },
+    { handler: 'throw', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'throw', matchId: 'ember-global' },
+    { handler: 'throw', matchId: 'deprecated-run-loop-and-computed-dot-access' },
   ],
 };
