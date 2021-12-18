98671ff5faef55451da79cbb6b6dfb080955955d
Phil Gengler <phil@pgengler.net>
2021-12-18 10:21:28 -0500

    Remove handled deprecations from deprecation-workflow.js

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,10 +1,5 @@
 /* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'warn', matchId: 'ember.built-in-components.import' },
-    { handler: 'throw', matchId: 'manager-capabilities.modifiers-3-13' },
-    { handler: 'throw', matchId: 'ember-global' },
-    { handler: 'throw', matchId: 'deprecated-run-loop-and-computed-dot-access' },
-  ],
+  workflow: [],
 };
