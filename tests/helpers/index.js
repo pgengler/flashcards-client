cec061eb0855532a1b59c895a465820430429565
Phil Gengler <phil@pgengler.net>
2025-06-28 09:15:33 -0400

    Run ember-cli-update to upgrade to 6.5.0

--- tests/helpers/index.js
+++ tests/helpers/index.js
@@ -1,9 +1,29 @@
+import {
+  setupApplicationTest as upstreamSetupApplicationTest,
+  setupRenderingTest as upstreamSetupRenderingTest,
+  setupTest as upstreamSetupTest,
+} from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 
-import { setupApplicationTest as upstreamSetupApplicationTest } from 'ember-qunit';
+// This file exists to provide wrappers around ember-qunit's
+// test setup functions. This way, you can easily extend the setup that is
+// needed per test type.
 
-export function setupApplicationTest(hooks, options) {
+function setupApplicationTest(hooks, options) {
   upstreamSetupApplicationTest(hooks, options);
   setupMirage(hooks);
 }
-export { setupTest, setupRenderingTest } from 'ember-qunit';
+
+function setupRenderingTest(hooks, options) {
+  upstreamSetupRenderingTest(hooks, options);
+
+  // Additional setup for rendering tests can be done here.
+}
+
+function setupTest(hooks, options) {
+  upstreamSetupTest(hooks, options);
+
+  // Additional setup for unit tests can be done here.
+}
+
+export { setupApplicationTest, setupRenderingTest, setupTest };
