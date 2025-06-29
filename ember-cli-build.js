da5bf8773c2c1231f6bf5c855a3b9f2f37ce4601
Phil Gengler <phil@pgengler.net>
2025-06-28 20:32:18 -0400

    Use version of bootstrap JS that includes popper

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -29,7 +29,7 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
 
   return app.toTree();
 };
