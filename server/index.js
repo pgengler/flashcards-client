6ac8afd47e7c2cd0b3334fca045904be610d1603
Phil Gengler <phil@pgengler.net>
2025-06-29 11:04:01 -0400

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -10,25 +10,16 @@
 const path = require('path');
 
 module.exports = function (app) {
-  console.log('loading proxies...');
-  try {
-    var globSync = require('glob').globSync;
-    console.log(__dirname);
-    console.log(globSync('./proxies/**/*.js', { cwd: __dirname }));
-    const paths = globSync('./proxies/**/*.js', { cwd: __dirname }).map((p) => path.join(__dirname, p));
-    console.log(paths);
+  const globSync = require('glob').globSync;
+  const paths = globSync('./proxies/**/*.js', { cwd: __dirname }).map((p) => path.join(__dirname, p));
 
-  var proxies = paths.map(require);
-
+  const proxies = paths.map(require);
 
   // Log proxy requests
-  var morgan = require('morgan');
+  const morgan = require('morgan');
   app.use(morgan('dev'));
 
   proxies.forEach(function (route) {
     route(app);
   });
-} catch (e) {
-  console.error(e);
-}
 };
