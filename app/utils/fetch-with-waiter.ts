e65e4570d73ca0c1abd678b73ee1af7a518f9b18
Phil Gengler <phil@pgengler.net>
2025-08-07 11:33:59 -0400

    Convert utils to TS

--- app/utils/fetch-with-waiter.ts
+++ app/utils/fetch-with-waiter.ts
@@ -2,10 +2,10 @@ import { buildWaiter } from '@ember/test-waiters';
 
 let waiter = buildWaiter('flashcards:fetch-waiter');
 
-export default async function _fetch() {
+export default async function _fetch(resource: RequestInfo | URL, options?: RequestInit) {
   let token = waiter.beginAsync();
   try {
-    let response = await fetch(...arguments);
+    let response = await fetch(resource, options);
     return response;
   } finally {
     waiter.endAsync(token);
